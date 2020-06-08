7a9f49cb29207f81b6202d423a0a2059c0c8dfb4
Eric Chen <exc5685@rit.edu>
2020-06-07 22:07:35 -0400

    update setup

--- setup3.ps1
+++ setup3.ps1
@@ -297,6 +297,15 @@ function Install-Rainway {
     Write-Output "Cleaning up Rainway installation file"
     Remove-Item -Path $downloadedFile -Confirm:$false
 }
+
+function Parsec-Cloud-Preperation-Tool {
+    [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"  
+    (New-Object System.Net.WebClient).DownloadFile("https://github.com/jamesstringerparsec/Parsec-Cloud-Preparation-Tool/archive/master.zip","$ENV:UserProfile\Downloads\Parsec-Cloud-Preparation-Tool.zip")  
+    New-Item -Path $ENV:UserProfile\Downloads\Parsec-Cloud-Preparation-Tool -ItemType Directory  
+    Expand-Archive $ENV:UserProfile\Downloads\Parsec-Cloud-Preparation-Tool.Zip -DestinationPath $ENV:UserProfile\Downloads\Parsec-Cloud-Preparation-Tool  
+    CD $ENV:UserProfile\Downloads\Parsec-Cloud-Preparation-Tool\Parsec-Cloud-Preparation-Tool-master\  
+    Powershell.exe -File $ENV:UserProfile\Downloads\Parsec-Cloud-Preparation-Tool\Parsec-Cloud-Preparation-Tool-master\Loader.ps1
+}
 InitDisks
 Update-Firewall
 Disable-Defender
@@ -319,5 +328,6 @@ Install-VirtualAudio
 Install-Steam
 Install-Parsec
 Install-Rainway
+Parsec-Cloud-Preperation-Tool
 # Add-AutoLogin $admin_username $admin_password
-Restart-Computer
+# Restart-Computer
